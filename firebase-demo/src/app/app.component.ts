62ce3741f6c1ab9f7d0ad1066b4adb7905fa1e2b
Nathan Staab <nstaab443@gmail.com>
2018-03-27 23:42:19 -0400

    Added work of reading and adding to a firebase list

--- firebase-demo/src/app/app.component.ts
+++ firebase-demo/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  courses;
+  course;
+  author;
+  coursesList: AngularFireList<{}>;
+
+  constructor(db: AngularFireDatabase) {
+    this.coursesList = db.list('/courses')
+    this.courses = db.list('/courses').valueChanges();
+    this.course = db.object('/courses/1').valueChanges();
+    this.author = db.object('/authors/1').valueChanges();
+  }
+
+  add(course2: HTMLInputElement){
+    this.coursesList.push(course2.value);
+    course2.value = "";
+  }
+
+  addObject(){
+    this.coursesList.push({
+      name: 'My Name',
+      price: 150,
+      isLive: true,
+      sections: [
+        { title: 'Components'},
+        { title: 'Directives'},
+        { title: 'Templates'}
+      ]
+
+    });
+    console.log(this.courses);
+  }
+
+
 }
--- firebase-demo/src/app/app.module.ts
+++ firebase-demo/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2'
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 
 import { AppComponent } from './app.component';
+import { environment } from './../environments/environment'
 
 
 @NgModule({
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AngularFireModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
